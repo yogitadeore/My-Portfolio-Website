64a58ec1b762225eb7879386f14c802eb5b0ce1f
Yogita deore <yogitadeore2001@gmail.com>
2025-04-01 08:15:37 +0530

    Fixed build issues: removed unused variables & fixed href

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div><footer>
-    <span>Created By <a href="#" className='main'>Yogita</a> | <span class="far fa-copyright"></span> 2025 All rights reserved.</span>
+    <span>Created By <a href="www.google.com" className='main'>Yogita</a> | <span class="far fa-copyright"></span> 2025 All rights reserved.</span>
 </footer>
 </div>
   )
